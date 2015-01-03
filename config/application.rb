6e0dbc01e33cdc990a96e4dd58e329cf8f463f00
SergeyKolenko <venty80@gmail.com>
2015-01-03 17:50:30 +0200

    Assets

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module MenuCalc
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.angular_templates.ignore_prefix  = %w(angular_app/templates/)
+
   end
 end
